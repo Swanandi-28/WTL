523c174cf934cf7d961509977a595ebbce9c3c70
Swanandi-28 <Swatik@0865>
2026-04-10 02:18:13 +0530

    php page for basic auth

---
+++ exp 7/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include "db.php";
+
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user'] = $user['name'];
+            header("Location: dashboard.php");
+        } else {
+            echo "Wrong password!";
+        }
+    } else {
+        echo "User not found!";
+    }
+}
+?>
+
+<form method="POST">
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit" name="login">Login</button>
+</form>
